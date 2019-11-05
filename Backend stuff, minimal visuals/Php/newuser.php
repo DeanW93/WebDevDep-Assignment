24072c5c71801c791f4197110fbbbd4a3a9f9da3
maciek <31696281+maciek98s@users.noreply.github.com>
2019-11-05 20:11:46 +0000

    Back end mechanics for Login authrisation Creating user and viewing records
    Code for to enter the user into the databses when he registeres and then login autherisation and then viewing personal details with minimal visual mostly just the backend mechanics

---
+++ Backend stuff, minimal visuals/Php/newuser.php
@@ -0,0 +1,47 @@
+
+<?php
+$db = mysqli_connect("localhost","root","","Project");
+//if(isset($_POST['submit'])){
+   // $name       = $_FILES['file']['name'];  
+    //$temp_name  = $_FILES['file']['tmp_name']; 
+//}
+
+
+
+$sql = "CREATE TABLE user (
+    firstname text(50),
+    Surname text(50),
+    Username text(50),
+    Password text(50),
+    Email text(50),
+    Address text(50),
+    Gender text(50),
+    ProfilePic text(100)
+)";
+
+ 	if ($db->query($sql) === TRUE) {
+    $sql = "INSERT INTO user (firstname,Surname,Username,Password,Email,Address,Gender,ProfilePic)
+VALUES ('$_POST[Name]', '$_POST[Surname]', '$_POST[Username]', '$_POST[Password]', '$_POST[Email]','$_POST[Address]','$_POST[Gender]','default.png')";
+} else {
+    $sql = "INSERT INTO user (firstname,Surname,Username,Password,Email,Address,Gender,ProfilePic)
+VALUES ('$_POST[Name]', '$_POST[Surname]', '$_POST[Username]', '$_POST[Password]', '$_POST[Email]','$_POST[Address]','$_POST[Gender]','default.png')";
+}
+
+
+
+
+
+
+
+
+if ($db->query($sql) == TRUE )
+{
+}
+
+/*
+$sql = "INSERT INTO usertable (Username, Password, First Name,Surname,Age)
+VALUES ('$_POST["username"]','$_POST["password"]','$_POST["firstname"]','$_POST["surname"],'$_POST["age"]')";
+*/	
+
+
+?>
