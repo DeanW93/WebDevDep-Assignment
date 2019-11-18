d63e4b25fc98b609eeddae8099995be934093f2b
Dean Whelan <deanwhelanhsd@hotmail.com>
2019-11-18 15:18:32 +0000

    added front end directory, added BS stylesheet, added jumbrotron banner on main html webpage

---
+++ Front-end framework/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+
+		<title>GameGo.com</title>
+
+		<!-- BootStrap Stylesheet -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	</head>
+
+
+	<body>
+		<div class="jumbotron bg-info text-light ml-3 mr-3 mt-3">
+			<h1><strong>GameGo</strong></h1>
+			<p>Official Website of Dean Whelan and Maciek Shipshinsky</p>
+		</div>
+
+	</body>
+
+
+</html>
