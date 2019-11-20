f45f13a857c34e54d30cd5b66d0caaf73ca8d813
Dean Whelan <deanwhelanhsd@hotmail.com>
2019-11-20 21:07:11 +0000

    ..

--- Front-end framework/index.php
+++ Front-end framework/index.php
@@ -11,15 +11,112 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+		<!-- JS used for modal -->
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  		<!-- JQuery -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
 	</head>
 
 
-	<body>
-		<div class="jumbotron bg-info text-light ml-3 mr-3 mt-3">
-			<h1><strong>GameGo</strong></h1>
-			<p>Official Website of Dean Whelan and Maciek Shipshinsky</p>
+	<body class="bg-success">
+
+		<!-- Jumbotron Banner -->
+		<div class="row jumbotron bg-light text-dark ml-3 mr-3 mt-3 rounded">
+			<div class="col">
+				<h1>
+					<strong>
+						Game<span class="text-success">Go</span>
+					</strong>
+				</h1>
+				<p>Official Website of Dean Whelan and Maciek Shipshinsky</p>
+			</div>
+			
 		</div>
 
+		<!-- NavBar -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light ml-3 mr-3 mt-3 rounded">
+  			<a class="navbar-brand" href="#">
+  				<img class="mr-3" src="images/gamepad-icon.jpg" width="30" height="30" alt="Pad"> GameGo
+  			</a>
+  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   				<span class="navbar-toggler-icon"></span>
+  			</button>
+
+  			<!-- Drop down menus -->
+	  		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	   			<ul class="navbar-nav mr-auto">
+
+	    			<li class="nav-item active">
+	        			<a class="nav-link text-success ml-3 mr-3" href="#">Home <span class="sr-only">(current)</span></a>
+	      			</li>
+
+	      			<li class="nav-item dropdown">
+	        			<a class="nav-link text-success dropdown-toggle ml-3 mr-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          				Platform
+	        			</a>
+	        			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				        	<a class="dropdown-item" href="#">PC</a>
+				        	<a class="dropdown-item" href="#">Playstation 4</a>
+				        	<a class="dropdown-item" href="#">Xbox 1</a>
+				        	<a class="dropdown-item" href="#">PS3/PS2</a>
+				        	<a class="dropdown-item" href="#">Xbox360/Xbox</a>
+				        	<a class="dropdown-item" href="#">Mobile</a>
+
+				        	<div class="dropdown-divider"></div>
+				        	<a class="dropdown-item" href="#">Browse by Platform</a>
+				        </div>
+	      			</li>
+
+	      			<li class="nav-item dropdown">
+	        			<a class="nav-link text-success dropdown-toggle ml-3 mr-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          				Genre
+	        			</a>
+	        			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				        	<a class="dropdown-item" href="#">Action/Adventure</a>
+				        	<a class="dropdown-item" href="#">First Person Shooter</a>
+				        	<a class="dropdown-item" href="#">Role Playing Games</a>
+				        	<a class="dropdown-item" href="#">Strategy</a>
+				        	<a class="dropdown-item" href="#">Simulation</a>
+				        	<a class="dropdown-item" href="#">Puzzle Games</a>
+				        	<a class="dropdown-item" href="#">VR Ready</a>
+
+				        	<div class="dropdown-divider"></div>
+				        	<a class="dropdown-item" href="#">Browse by Genre</a>
+				        </div>
+	      			</li>
+
+	      			<li class="nav-item dropdown">
+	        			<a class="nav-link text-success dropdown-toggle ml-3 mr-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          				Price Range
+	        			</a>
+	        			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				        	<a class="dropdown-item" href="#">€5 - €10</a>
+				        	<a class="dropdown-item" href="#">€10 - €20</a>
+				        	<a class="dropdown-item" href="#">€20 - €40</a>
+				        	<a class="dropdown-item" href="#">€40 - €60</a>
+				        	<a class="dropdown-item" href="#">€60+ </a>
+				        	<div class="dropdown-divider"></div>
+				        	<a class="dropdown-item" href="#">Browse by price</a>
+				        </div>
+	      			</li>
+	    		</ul>
+
+
+
+	    		<!-- Search Form -->
+	    		<form class="form-inline my-2 my-lg-6">
+	    			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+	    			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+	    		</form>
+			</div>
+		</nav>
+
+		
 	</body>
 
 
